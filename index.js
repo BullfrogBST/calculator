44573974fbe63328412c7caba286bacdd7afd6ca
Michaela Beakley <bullfrogbst@gmail.com>
2022-07-17 16:59:34 -0500

    Make (faulty) operate function

--- index.js
+++ index.js
@@ -6,28 +6,30 @@ const enterBtn = document.querySelector('.enter-btn');
 //Create an array for the numbers and operators entered, and one for the whole input
 let inputArray = [];
 let tempInput = [];
+
+let output = '';
 //Create the add function that takes an array
 function add(arr){
     return arr.reduce((sum, item) =>{
-        return sum + item;
+        return parseInt(sum) + parseInt(item);
     });
 };
 //Create the subtract function that takes an array
 function subtract(arr){
     return arr.reduce((result, item) =>{
-        return result - item;
+        return parseInt(result) - parseInt();
     });
 };
 //Create the multiply function that takes an array
 function multiply(arr){
     return arr.reduce((product, item) =>{
-        return product * item;
+        return parseInt(product) * parseInt(item);
     });
 };
 //Create the divide function that takes an array
 function divide(arr){
     return arr.reduce((quotient, item) =>{
-        return quotient / item;
+        return parseInt(quotient) / parseInt(item);
     });
 };
 //Every time a button is pressed, send the cooresponding value to the input array and display it to the output box
@@ -58,18 +60,22 @@ opBtns.forEach(button =>{
         switch(e.target.getAttribute('id')){
             case 'plus': outputBar.textContent = '';
             inputArray.push(tempInput.join(''), '+');
+            tempInput = [];
             outputLength = 0;
             break;
             case 'minus': outputBar.textContent = '';
             inputArray.push(tempInput.join(''), '-');
+            tempInput = [];
             outputLength = 0;
             break;
             case 'times': outputBar.textContent = '';
             inputArray.push(tempInput.join(''), '*');
+            tempInput = [];
             outputLength = 0;
             break;
             case 'divide': outputBar.textContent = '';
             inputArray.push(tempInput.join(''), '/');
+            tempInput = [];
             outputLength = 0;
             break;
         }
@@ -79,18 +85,22 @@ document.addEventListener('keydown', (e) =>{
     switch(e.key){
         case '+': outputBar.textContent = '';
         inputArray.push(tempInput.join(''), '+');
+        tempInput = [];
         outputLength = 0;
         break;
         case '-': outputBar.textContent = '';
         inputArray.push(tempInput.join(''), '-');
+        tempInput = [];
         outputLength = 0;
         break;
         case '*': outputBar.textContent = '';
         inputArray.push(tempInput.join(''), '*');
+        tempInput = [];
         outputLength = 0;
         break;
         case '/': outputBar.textContent = '';
         inputArray.push(tempInput.join(''), '/');
+        tempInput = [];
         outputLength = 0;
         break;
     }
@@ -98,25 +108,39 @@ document.addEventListener('keydown', (e) =>{
 
 //Every time the enter button is pressed, or when enter is pressed, call the operate function
 enterBtn.addEventListener('click', () =>{
+    inputArray.push(tempInput.join());
+    tempInput = [];
     operate();
 })
 
 document.addEventListener('keydown', (e) =>{
     if(e.key = 'enter'){
+        inputArray.push(tempInput.join());
+        tempInput = [];
         operate();
     }
 })
 //Declare the operate function that takes the input array
 function operate(){
-    console.log('Hooty!!')
+    let markedIndex = 0;
+    //For each item in the input array, if the next item is an operator, then add the previous number to an array
+    for(let i=0; i<inputArray.length; i++){
+        //Call the function that the operator matches
+        switch(inputArray[i][0]){
+            case '*': outputBar.textContent = multiply([inputArray[i-1], inputArray[i+1]])
+            case '/': outputBar.textContent = divide([inputArray[i-1], inputArray[i+1]])
+        }
+    }
+    for(let i=0; i<inputArray.length; i++){
+        //Call the function that the operator matches
+        switch(inputArray[i][0]){
+            case '+': outputBar.textContent = add([inputArray[i-1], inputArray[i+1]]);
+            case '-': outputBar.textContent = subtract([inputArray[i-1], inputArray[i+1]]);
+        }
+    }
+    //If the input array's empty, display the result to the output box and exit the function
+    
+    //If there's any more items in the array, repeat the process
 }
-//For each item in the input array, if the next item is an operator, then add the previous number to an array
-
-//Call the function that the operator matches
-
-//If the input array's empty, display the result to the output box and exit the function
-
-//If there's any more items in the array, repeat the process
-
 
 //If the clear button is pressed, clear the output box.
