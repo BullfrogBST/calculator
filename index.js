33318120541eadfaf76e9b88abcda91068807e9c
Michaela Beakley <bullfrogbst@gmail.com>
2022-07-17 13:42:34 -0500

    Add JS pseudocode

---
+++ index.js
@@ -0,0 +1,15 @@
+//Create references to the number buttons, operator buttons, and output box
+
+//Create an array for the numbers and operators entered, and when joined will make an equation
+
+//Create the add function that takes an array
+
+//Create the subtract function that takes an array
+
+//Create the multiply function that takes an array
+
+//Create the divide function that takes an array
+
+//Create the operate function that takes the input array
+
+//For each item in the input
