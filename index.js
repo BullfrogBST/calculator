949b6e45a271221e7a8a24b2a90a16325edca7da
Michaela Beakley <bullfrogbst@gmail.com>
2022-07-18 17:23:23 -0500

    Add the getEquation() function

--- index.js
+++ index.js
@@ -117,7 +117,8 @@ enterBtn.addEventListener('click', () =>{
 })
 
 document.addEventListener('keyup', (e) =>{
-    if(e.key == 'enter'){
+    if(e.code == 'Enter'){
+        console.log('Hootsafer')
         inputArray.push(tempInput.join());
         tempInput = [];
         operate();
@@ -126,8 +127,9 @@ document.addEventListener('keyup', (e) =>{
 //Declare the operate function that takes an operator and two numbers
 function operate(){
     //Declare an array that is equal to the return value of getEquation()
-
-    //For each item in the equations array, check what the operator is and if it's '*' or '/', call the appropriate function on the numbers in the array. Set the output value to the answer
+    let equations = getEquation();
+    console.log(equations);
+    //For each item in the equations array, check what the operator is and if it's '*' or '/', call the appropriate function on the numbers in the array.
 
     //Then, do the same thing again but check if the operator is '+' or '-'
 
@@ -135,6 +137,20 @@ function operate(){
     
 }
 
+//Declare getEquation(), which returns an array of objects with operator, num1, and num2 as properties
+function getEquation(){
+    let equationsArr = [];
+    for(let i=0; i<inputArray.length; i++){
+        if(inputArray[i] == '+' || inputArray[i] == '-' || inputArray[i] == '*' || inputArray[i] == '/'){
+            equationsArr.push({ 
+                num1: parseFloat(inputArray[i-1]),
+                operator: inputArray[i],
+                num2: parseFloat(inputArray[i+1])
+            })
+        }
+    }
+    return equationsArr;
+}
 //If the clear button is pressed, clear the output value, inputArray, and tempInput.
 
 //Declare the output() function, which makes the outputBar's textContent the output
