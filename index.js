fb19d63483d6df04e2139b537597032b81dcd6cc
Michaela Beakley <bullfrogbst@gmail.com>
2022-07-18 17:00:08 -0500

    Update pseudocode in preperation for minor rewrite

--- index.js
+++ index.js
@@ -9,30 +9,23 @@ let tempInput = [];
 
 let output = '';
 //Create the add function that takes an array
-function add(arr){
-    return arr.reduce((sum, item) =>{
-        return parseInt(sum) + parseInt(item);
-    });
-};
+function add(num1, num2){
+    console.log('added')
+    return parseFloat(num1) + parseFloat(num2);
+}
 //Create the subtract function that takes an array
-function subtract(arr){
-    return arr.reduce((result, item) =>{
-        return parseInt(result) - parseInt();
-    });
-};
+function subtract(num1, num2){
+    return parseFloat(num1) - parseFloat(num2);
+}
 //Create the multiply function that takes an array
-function multiply(arr){
-    return arr.reduce((product, item) =>{
-        return parseInt(product) * parseInt(item);
-    });
-};
+function multiply(num1, num2){
+    return parseFloat(num1) * parseFloat(num2);
+}
 //Create the divide function that takes an array
-function divide(arr){
-    return arr.reduce((quotient, item) =>{
-        return parseInt(quotient) / parseInt(item);
-    });
-};
-//Every time a button is pressed, send the cooresponding value to the input array and display it to the output box
+function divide(num1, num2){
+    return parseFloat(num1) / parseFloat(num2);
+}
+//Every time a button is pressed, send the cooresponding value to the input array and set the output value to the button pressed
 let outputLength = 1;
 numBtns.forEach(button =>{
     button.addEventListener('click', (e) =>{
@@ -113,34 +106,25 @@ enterBtn.addEventListener('click', () =>{
     operate();
 })
 
-document.addEventListener('keydown', (e) =>{
-    if(e.key = 'enter'){
+document.addEventListener('keyup', (e) =>{
+    if(e.key == 'enter'){
         inputArray.push(tempInput.join());
         tempInput = [];
         operate();
     }
 })
-//Declare the operate function that takes the input array
+//Declare the operate function that takes an operator and two numbers
 function operate(){
-    let markedIndex = 0;
-    //For each item in the input array, if the next item is an operator, then add the previous number to an array
-    for(let i=0; i<inputArray.length; i++){
-        //Call the function that the operator matches
-        switch(inputArray[i][0]){
-            case '*': outputBar.textContent = multiply([inputArray[i-1], inputArray[i+1]])
-            case '/': outputBar.textContent = divide([inputArray[i-1], inputArray[i+1]])
-        }
-    }
-    for(let i=0; i<inputArray.length; i++){
-        //Call the function that the operator matches
-        switch(inputArray[i][0]){
-            case '+': outputBar.textContent = add([inputArray[i-1], inputArray[i+1]]);
-            case '-': outputBar.textContent = subtract([inputArray[i-1], inputArray[i+1]]);
-        }
-    }
-    //If the input array's empty, display the result to the output box and exit the function
+    //Declare an array that is equal to the return value of getEquation()
+
+    //For each item in the equations array, check what the operator is and if it's '*' or '/', call the appropriate function on the numbers in the array. Set the output value to the answer
+
+    //Then, do the same thing again but check if the operator is '+' or '-'
+
+    //Call the output() function
     
-    //If there's any more items in the array, repeat the process
 }
 
 //If the clear button is pressed, clear the output box.
+
+//Declare the output() function, which makes the outputBar's textContent the output
