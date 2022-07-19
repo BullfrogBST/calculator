e44c92210f267143238d9766e85e82ad98b23deb
Michaela Beakley <bullfrogbst@gmail.com>
2022-07-19 07:01:01 -0500

    Rewrite operate function; still bug in getEquation()

--- index.js
+++ index.js
@@ -11,19 +11,19 @@ let outputValue = '';
 //Create the add function that takes an array
 function add(num1, num2){
     console.log('added')
-    return parseFloat(num1) + parseFloat(num2);
+    return num1 + num2;
 }
 //Create the subtract function that takes an array
 function subtract(num1, num2){
-    return parseFloat(num1) - parseFloat(num2);
+    return num1 - num2;
 }
 //Create the multiply function that takes an array
 function multiply(num1, num2){
-    return parseFloat(num1) * parseFloat(num2);
+    return num1 * num2;
 }
 //Create the divide function that takes an array
 function divide(num1, num2){
-    return parseFloat(num1) / parseFloat(num2);
+    return num1 / num2;
 }
 //Every time a button is pressed, send the cooresponding value to the input array and set the output value to the button pressed
 let outputLength = 1;
@@ -36,6 +36,7 @@ numBtns.forEach(button =>{
             output();
             outputLength++
         }
+        console.log(tempInput)
     })
 })
 document.addEventListener('keydown', (e) =>{
@@ -118,7 +119,6 @@ enterBtn.addEventListener('click', () =>{
 
 document.addEventListener('keyup', (e) =>{
     if(e.code == 'Enter'){
-        console.log('Hootsafer')
         inputArray.push(tempInput.join());
         tempInput = [];
         operate();
@@ -129,16 +129,51 @@ function operate(){
     //Declare an array that is equal to the return value of getEquation()
     let equations = getEquation();
     console.log(equations);
-    //For each item in the equations array, check what the operator is and if it's '*' or '/', call the appropriate function on the numbers in the array.
-
+    //For each item in the equations array, check what the operator is and if it's '*' or '/', call the appropriate function on the numbers in the array. Make the outputValue the answer, and make num1 of the next equation the answer
+    let answer = null;
+    for(let i=0; i<equations.length; i++){
+        //If the previous answer isn't null, make that the value of num1
+        if(answer != null){
+            equations[i].num1 = answer;
+        }
+        switch(equations[i].operator){
+            case '*': answer = multiply(equations[i].num1, equations[i].num2)
+            finalizeEquation(answer);
+            break;
+            case '/': answer = divide(equations[i].num1, equations[i].num2)
+            finalizeEquation()
+            console.log(answer);
+            break;
+        }
+    }
     //Then, do the same thing again but check if the operator is '+' or '-'
+    for(let i=0; i<equations.length; i++){
+        //If the previous answer isn't null, make that the value of num1
+        if(answer != null){
+            equations[i].num1 = answer;
+        }
+        switch(equations[i].operator){
+            case '+': answer = add(equations[i].num1, equations[i].num2)
+            finalizeEquation()
+            console.log(answer);
+            break;
+            case '/': answer = subtract(equations[i].num1, equations[i].num2)
+            finalizeEquation()
+            console.log(answer);
+            break;
+        }
+    }
 
-    //Call the output() function
-    
+    //Declare the finalizeEquation() function. THis function simply prevents repeated code when solving the simplified equations above
+    function finalizeEquation(answer){
+        outputValue = answer;
+        output()
+    }
 }
 
 //Declare getEquation(), which returns an array of objects with operator, num1, and num2 as properties
 function getEquation(){
+    console.log(inputArray);
     let equationsArr = [];
     for(let i=0; i<inputArray.length; i++){
         if(inputArray[i] == '+' || inputArray[i] == '-' || inputArray[i] == '*' || inputArray[i] == '/'){
