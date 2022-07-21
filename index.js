4f39d3d0fb1774228019268ab930bf419b0d8542
Michaela Beakley <bullfrogbst@gmail.com>
2022-07-21 08:38:17 -0500

    Begin to rewrite operate() again

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ let tempInput = [];
 let outputValue = '';
 //Create the add function that takes an array
 function add(num1, num2){
-    console.log('added')
     return num1 + num2;
 }
 //Create the subtract function that takes an array
@@ -82,86 +81,75 @@ opBtns.forEach(button =>{
     })
 })
 document.addEventListener('keydown', (e) =>{
-    switch(e.key){
-        case '+': outputValue = '';
-        output();
-        inputArray.push(tempInput.join(''), '+');
-        tempInput = [];
-        outputLength = 0;
-        break;
-        case '-': outputValue = '';
-        output();
-        inputArray.push(tempInput.join(''), '-');
-        tempInput = [];
-        outputLength = 0;
-        break;
-        case '*': outputValue = '';
-        output();
-        inputArray.push(tempInput.join(''), '*');
-        tempInput = [];
-        outputLength = 0;
-        break;
-        case '/': outputValue = '';
-        output();
-        inputArray.push(tempInput.join(''), '/');
-        tempInput = [];
-        outputLength = 0;
-        break;
+    if(isOperator(e.code)){
+        switch(e.key){
+            case '+': outputValue = '';
+            output();
+            inputArray.push(tempInput.join(''), '+');
+            tempInput = [];
+            outputLength = 0;
+            break;
+            case '-': outputValue = '';
+            output();
+            inputArray.push(tempInput.join(''), '-');
+            tempInput = [];
+            outputLength = 0;
+            break;
+            case '*': outputValue = '';
+            output();
+            inputArray.push(tempInput.join(''), '*');
+            tempInput = [];
+            outputLength = 0;
+            break;
+            case '/': outputValue = '';
+            output();
+            inputArray.push(tempInput.join(''), '/');
+            tempInput = [];
+            outputLength = 0;
+            break;
+        }
     }
 })
 
 //Every time the enter button is pressed, or when enter is pressed, call the operate function
 enterBtn.addEventListener('click', () =>{
-    inputArray.push(tempInput.join());
+    inputArray.push(tempInput.join(''));
     tempInput = [];
     operate();
 })
 
 document.addEventListener('keyup', (e) =>{
     if(e.code == 'Enter'){
-        inputArray.push(tempInput.join());
-        tempInput = [];
-        operate();
+    inputArray.push(tempInput.join(''));
+    tempInput = [];
+    operate();
     }
 })
 //Declare the operate function that takes an operator and two numbers
 function operate(){
-    //Declare an array that is equal to the return value of getEquation()
-    let equations = getEquation();
-    console.log(equations);
-    //For each item in the equations array, check what the operator is and if it's '*' or '/', call the appropriate function on the numbers in the array. Make the outputValue the answer, and make num1 of the next equation the answer
-    let answer = null;
-    for(let i=0; i<equations.length; i++){
-        //If the previous answer isn't null, make that the value of num1
-        if(answer != null){
-            equations[i].num1 = answer;
+    //Make an array with the equations, using the getEquation() array
+
+    //Use a for loop to implement order of operations, and while the input has the operand, perform the according function on it. Set the outputValue to the answer
+
+    //Declare getEquation(), which returns an array of objects with operator, num1, and num2 as properties
+    function getEquation(){
+        console.log(inputArray)
+        //Declare equations array
+        const equations = [];
+        for(let i=0; i<inputArray.length; i++){
+            //Check if the current index in the array is an operator
+            if(isOperator(inputArray[i])){
+                console.log(inputArray[i-1]);
+                //Push an object with the number before the operator as "num1", the operator as "operator", and the number after the operator as "num2"
+                equations.push({
+                    num1: parseFloat(inputArray[i-1]),
+                    operator: inputArray[i],
+                    num2: parseFloat(inputArray[i+1])
+                })
+            }
         }
-        switch(equations[i].operator){
-            case '*': answer = multiply(equations[i].num1, equations[i].num2)
-            finalizeEquation(answer);
-            break;
-            case '/': answer = divide(equations[i].num1, equations[i].num2)
-            finalizeEquation()
-            console.log(answer);
-            break;
-        }
-    }
-    //Then, do the same thing again but check if the operator is '+' or '-'
-    for(let i=0; i<equations.length; i++){
-        //If the previous answer isn't null, make that the value of num1
-        if(answer != null){
-            equations[i].num1 = answer;
-        }
-        switch(equations[i].operator){
-            case '+': answer = add(equations[i].num1, equations[i].num2)
-            finalizeEquation()
-            console.log(answer);
-            break;
-            case '/': answer = subtract(equations[i].num1, equations[i].num2)
-            finalizeEquation()
-            console.log(answer);
-            break;
-        }
+        //Return the equations array
+        return equations;
     }
 
     //Declare the finalizeEquation() function. THis function simply prevents repeated code when solving the simplified equations above
@@ -171,25 +159,14 @@ function operate(){
     }
 }
 
-//Declare getEquation(), which returns an array of objects with operator, num1, and num2 as properties
-function getEquation(){
-    console.log(inputArray);
-    let equationsArr = [];
-    for(let i=0; i<inputArray.length; i++){
-        if(inputArray[i] == '+' || inputArray[i] == '-' || inputArray[i] == '*' || inputArray[i] == '/'){
-            equationsArr.push({ 
-                num1: parseFloat(inputArray[i-1]),
-                operator: inputArray[i],
-                num2: parseFloat(inputArray[i+1])
-            })
-        }
-    }
-    return equationsArr;
+
+//Declare isOperator, which just returns true if the argument is an operator
+function isOperator(item){
+    return item == '+' || item == '-' || item == '*' || item == '/';
 }
 //If the clear button is pressed, clear the output value, inputArray, and tempInput.
 
 //Declare the output() function, which makes the outputBar's textContent the output
 function output(){
-    console.log('output')
     outputBar.textContent = outputValue;
 }
